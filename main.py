fe1a5831d29c88348e19330a7d9c9ca409b81dc6
JAkhimiemona40 <70667425+JAkhimiemona40@users.noreply.github.com>
2021-02-22 22:00:04 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,163 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+
+input('Welcome to Millionaire: 2020 NBA EDITION! What is your name?')
+
+keep_playing = True
+num = 0
+num2 = str(num)
+print('Currently you have made $', num)
+
+
+print('Who was the 2020 NBA KIA MVP?')
+print('a. Giannis Antetokounmpo')
+print('b. LeBron James')
+print('c. James Harden')
+print('d. Anthony Davis')
+user_answer1 = str(input(">> "))
+
+num = 500
+num2 = str(num)
+
+if user_answer1 == 'a':
+    print('Correct!')
+    print('Currently you have made $' , num2)
+else:
+    print("I'm sorry, that is incorrect.")
+    keep_playing = False
+    
+if keep_playing == True:
+    print('Which NBA player is big on BLM?')
+    print('a. LeBron James')
+    print('b. Ja Morant')
+    print('c. Kyrie Irving')
+    print('d. Jaylen Brown')
+    user_answer1 = str(input(">> "))
+    
+    num = 1000
+    num3 = str(num)
+    
+    if user_answer1 == 'c':
+        print('Correct!')
+        print('Currently you have made $' , num3)
+    else:
+        print("I'm sorry, that is incorrect.")
+        keep_playing = False
+
+if keep_playing == True:
+    print('Who is the star player of the Boston Celtics?')
+    print('a. Larry Bird')
+    print('b. Jayson Tatum')
+    print('c. Daniel Theis')
+    print('d. Jaylen Brown')
+    user_answer1 = str(input(">> "))
+    
+    num = 5000
+    num4 = str(num)
+    
+    if user_answer1 == 'b':
+        print('Correct!')
+        print('Currently you have made $' , num4)
+    else:
+        print("I'm sorry, that is incorrect.")
+        keep_playing = False
+
+if keep_playing == True:
+    print('Who won the 2020 NBA Championship?')
+    print('a. Los Angeles Lakers')
+    print('b. Toronto Raptors')
+    print('c. Miami Heat')
+    print('d. Minnesota Timberwolves')
+    user_answer1 = str(input(">> "))
+    
+    num = 20000
+    num5 = str(num)
+    
+    if user_answer1 == 'a':
+        print('Correct!')
+        print('Currently you have made $' , num5)
+    else:
+        print("I'm sorry, that is incorrect.")
+        keep_playing = False
+        
+if keep_playing == True:
+    print('Who was the 3rd pick in the 2020 NBA Draft?')
+    print('a. DeAndre Ayton')
+    print('b. Tyrese Haliburton')
+    print('c. Lonzo Ball')
+    print('d. Lamelo Ball')
+    user_answer1 = str(input(">> "))
+    
+    num = 50000
+    num6 = str(num)
+    
+    if user_answer1 == 'd':
+        print('Correct!')
+        print('Currently you have made $' , num6)
+    else:
+        print("I'm sorry, that is incorrect.")
+        keep_playing = False
+
+if keep_playing == True:
+    print('Which NBA star is nicknamed, "The Joker"?')
+    print('a. Nikola Jokic')
+    print('b. Joel Embiid')
+    print('c. Michael Jordan')
+    print('d. Donovan Mitchell')
+    user_answer1 = str(input(">> "))
+    
+    num = 250000
+    num7 = str(num)
+    
+    if user_answer1 == 'a':
+        print('Correct!')
+        print('Currently you have made $' , num7)
+    else:
+        print("I'm sorry, that is incorrect.")
+        keep_playing = False
+        
+if keep_playing == True:
+    print('What NBA team is the closest to the Nations Capitol?')
+    print('a. New York Knicks')
+    print('b. Washington Wizards')
+    print('c. Philadelphia Sixers')
+    print('d. Cleveland Cavaliers')
+    user_answer1 = str(input(">> "))
+    
+    num = 500000
+    num8 = str(num)
+    
+    if user_answer1 == 'b':
+        print('Correct!')
+        print('Currently you have made $' , num8)
+    else:
+        print("I'm sorry, that is incorrect.")
+        keep_playing = False
+        
+if keep_playing == True:
+    print('Which NBA team did NBA legend, Dwayne Wade, play for majority of his career?')
+    print('a. Brooklyn Nets')
+    print('b. Utah Jazz')
+    print('c. Portland Trail Blazers')
+    print('d. Miami Heat')
+    user_answer1 = str(input(">> "))
+    
+    num = 1000000
+    num9 = str(num)
+    
+    if user_answer1 == 'd':
+        print('Correct!')
+        print('Congratulations, you are a millionaire!!! You have officially made $' , num9)
+    else:
+        print("I'm sorry, that is incorrect.")
+        keep_playing = False
+    
+    
+
+
